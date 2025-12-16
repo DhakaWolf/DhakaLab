ba78ae2b2ceb1ef576ce538a5f772be8c39e3c1b
Dhaka Wolf <th.rage.of.music@gmail.com>
2025-12-16 16:56:45 -0600

    Added Test-VMWorkstationStatus.ps1 and updated some READMEs

---
+++ Automation/README.md
@@ -0,0 +1,4 @@
+# Automation
+
+## TRIGGERcmd
+I use [TRIGGERcmd](https://triggercmd.com/en/) to run scripts/commands against my Windows devices via Google Home or iOS Shortcuts.
---
+++ Automation/TRIGGERcmd/Test-VMWorkstationStatus.ps1
@@ -0,0 +1,13 @@
+try {
+    $objProc = Get-Process -Name 'vmware.exe'
+    If ($objProc){
+        If ($objProc.Responding -eq $true){
+            Write-Output 'Workstation is currently running.'
+        } Else {
+            Write-Output 'Workstation is currently not running or not responding.'
+        }
+    }
+}
+catch {
+    Write-Output "There was a problem checking the status of VMware Workstation on $env:HOSTNAME"
+}
